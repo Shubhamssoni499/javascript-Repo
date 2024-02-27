efada9b85e3738e398d7e2dede2fe467c08fb28b
Shubhamssoni499 <141657285+Shubhamssoni499@users.noreply.github.com>
2024-02-27 09:47:46 +0000

    datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treeat all js code as newer version of JS
+
+//alert(3+3) // we are using nodejs, not browser 
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53 
+//bigint
+//string => ""
+//boolean ==> true/false
+//null => standalone value
+//undefined=>
+//symbol=> unique
+
+
+//object
+console.log(typeof "Hitesh");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
