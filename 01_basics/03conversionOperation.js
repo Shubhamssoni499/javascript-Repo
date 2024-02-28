4488427c82ca247c553a5ec7bb36cb6b7bf84636
Shubhamssoni499 <141657285+Shubhamssoni499@users.noreply.github.com>
2024-02-28 07:17:23 +0000

    conversionofdatatypes

---
+++ 01_basics/03conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "shubham"
+// console.log(typeof score);
+// //console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// "true" =>1; false =>0
+
+// let isLoggedIn = "ssss123"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = "123shubham"
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
