f827d0ccdc24bc9f5e74f678fef41557e77bf4e9
Shubhamssoni499 <141657285+Shubhamssoni499@users.noreply.github.com>
2024-02-28 09:45:56 +0000

    comparisions

---
+++ 01_basics/04comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(2 < "22");
+// console.log("shubh">22);
+// console.log("shubh"<22);
+// console.log("shubhsjyrgfourgtbbbb">1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// avoid all these types of conversions
+
+// ====
+
+console.log("2" === 2)
