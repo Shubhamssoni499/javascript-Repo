9126a90161c3556dc877ed0aa164e4c53497a929
Shubhamssoni499 <141657285+Shubhamssoni499@users.noreply.github.com>
2024-02-28 08:33:46 +0000

    mdn

--- 01_basics/03conversionOperation.js
+++ 01_basics/03conversionOperation.js
@@ -19,7 +19,56 @@
 //"" => false
 // "hitesh" => true
 
-let someNumber = "123shubham"
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = "123shubham"
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************** Operations *******************************
+
+let value = 3
+let negaValue = -value 
+//console.log(negaValue)
+//console.log(typeof negaValue);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2 
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(-true);
+// console.log(+"");
+// console.log(-"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// //gameCounter++;
+// ++gameCounter
+// console.log(gameCounter);
+
+//*****************MDN********************
+
+// The increment (++) operator increments (adds one to) its operand and returns 
+// the value before or after the increment,
+// depending on where the operator is placed 
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
